ee246183b557ffd1a2ab58ebfcf500ec1d05c389
Damien Tillet <tyes666@free.fr>
2021-02-22 11:07:18 +0100

    modifs

--- panier.js
+++ panier.js
@@ -1,5 +1,6 @@
+
+
 let teddySelected = JSON.parse(localStorage.getItem("teddySelected"));
-console.log(teddySelected);
 
 let container = document.getElementById("tedd");
 let elementDiv = document.createElement("div");
@@ -7,46 +8,53 @@ elementDiv.id="resume";
 elementDiv.classList.add("card");
 container.appendChild(elementDiv);
 
-let containerImg = document.getElementById("resume");
+for (let i=0; i<teddySelected.length; i++){
+
+
+let contenant = document.getElementById("resume");
+let contenantProduct = document.createElement("div");
+contenant.appendChild(contenantProduct);
+
+let resume = document.getElementById("resume");
 let oursImg = document.createElement("img");
-let image = localStorage.getItem("teddySelected");
-oursImg.id="oursImg";
-oursImg.classList.add("articlepanier","col-2");
-oursImg.src = (teddySelected.name);
-containerImg.appendChild(oursImg);
+let image = localStorage.getItem('imageUrl');
+//oursImg.id="oursImg";
+oursImg.classList.add("articlepanier","col-12","oursImg");
+oursImg.src = teddySelected[i].imageUrl;
+contenantProduct.appendChild(oursImg);
 
 let oursResume = document.createElement("div");
-oursResume.id="oursResume";
-containerImg.appendChild(oursResume);
+//oursResume.id="oursResume";
+oursResume.classList.add("oursResume","col-2");
+contenant.appendChild(oursResume);
 
-let containerName = document.getElementById("oursResume");    
+   
+let containerProduct = document.getElementsByClassName("oursResume");
 let oursName = document.createElement("div");
 let name = localStorage.getItem("name");
 oursName.classList.add("name");
-oursName.textContent = `${name}`;
-containerName.appendChild(oursName);
+oursName.textContent = teddySelected[i].name;
+oursResume.appendChild(oursName);
 
-let containerColor = document.getElementById("oursResume");    
-let oursColor = document.createElement("div");
-let colors = localStorage.getItem("colors");
-oursColor.textContent = `${colors}`;
-containerColor.appendChild(oursColor);
+
 
 let containerPrice = document.getElementById("resume");
 let oursPrice = document.createElement("div");
 let price = localStorage.getItem("price");
-oursPrice.textContent = `${price / 100 + " €uros"}`;
+oursPrice.classList.add("col-2")
+oursPrice.id="price";
+oursPrice.textContent = teddySelected[i].price / 100 + " €uros";
 containerPrice.appendChild(oursPrice);
 
 let countEl = document.getElementById("count");
 function plus(){
-    oursQte.value ++;
-countEl.value = "oursQte";
+oursQte.value ++;
+countEl.value = oursQte;
 }
 function minus(){
 if (oursQte => 0) {
-    oursQte.value --;
-countEl.value = "oursQte";
+oursQte.value --;
+countEl.value = oursQte;
 }
 }
 
@@ -54,23 +62,160 @@ let oursMinus = document.createElement("input");
 oursMinus.value= "-";
 oursMinus.type= "button";
 oursMinus.addEventListener("click", minus);
-containerImg.appendChild(oursMinus)
+contenant.appendChild(oursMinus)
+
 let oursPlus = document.createElement("input");
 oursPlus.value= "+";
 oursPlus.type= "button";
 oursPlus.addEventListener("click", plus);
-containerImg.appendChild(oursPlus)
+contenant.appendChild(oursPlus)
 
 let oursQte = document.createElement("input");
 oursQte.id="count";
 oursQte.value = "1";
-containerImg.appendChild(oursQte);
+contenant.appendChild(oursQte);
 
 localStorage.setItem("qte", oursQte.value);
 
 let containerTotal = document.getElementById("resume");
 let priceTotal = document.createElement("div");
+priceTotal.value="count * price";
 containerTotal.appendChild(priceTotal);
+
+
+}
+
+
+
+
+let nameText = document.createElement("label");
+nameText.classList.add("userName","formulaire");
+nameText.textContent = "Veuillez entrer votre nom";
+container.appendChild(nameText);
+
+let nameUser = document.createElement("input");
+nameUser.id="nameUser";
+container.appendChild(nameUser);
+
+let firstNameText = document.createElement("label")
+firstNameText.classList.add("userFirstName","formulaire")
+firstNameText.textContent = "Veuillez entrer votre prénom";
+container.appendChild(firstNameText);
+
+let firstNameUser = document.createElement("input");
+firstNameUser.id="firstNameUser";
+container.appendChild(firstNameUser);
+
+let adresseText = document.createElement("label")
+adresseText.classList.add("userAdresse","formulaire")
+adresseText.textContent = "Veuillez entrer votre adresse";
+container.appendChild(adresseText);
+
+let adresseUser = document.createElement("input");
+adresseUser.id="adresseUser"
+container.appendChild(adresseUser);
+
+let cpText = document.createElement("label")
+cpText.classList.add("userCp","formulaire")
+cpText.textContent = "Veuillez entrer votre code postal";
+container.appendChild(cpText);
+
+let cpUser = document.createElement("input");
+cpUser.id="cpUser";
+container.appendChild(cpUser);
+
+let villeText = document.createElement("label")
+villeText.classList.add("userVille","formulaire")
+villeText.textContent = "Veuillez entrer votre ville";
+container.appendChild(villeText);
+
+let villeUser = document.createElement("input");
+villeUser.id="userVille";
+container.appendChild(villeUser);
+
+let mailText = document.createElement("label")
+mailText.classList.add("userMail","formulaire")
+mailText.textContent = "Veuillez entrer votre email";
+container.appendChild(mailText);
+
+let mailUser = document.createElement("input");
+mailUser.id="userMail";
+container.appendChild(mailUser);
+
+
+let buttonCommand = document.createElement("button");
+buttonCommand.classList.add("btn","btn-primary","btn-commande");
+buttonCommand.id="btn-envoi";
+buttonCommand.type = "submit";
+buttonCommand.textContent ="Commander";
+container.appendChild(buttonCommand);
+
+
+
+
+let contact = {
+    
+    userName : document.getElementById("nameUser").value,
+    userFirstName: document.getElementById("firstNameUser").value,
+    userAddress: document.getElementById("adresseUser").value,
+    userCp: document.getElementById("cpUser").value,
+    userVille: document.getElementById("userVille").value,
+    userMail: document.getElementById("userMail").value,
+  };
+ 
+let sendCommand = document.getElementById("btn-envoi");
+sendCommand.addEventListener("click", function addUser (){
+    function remplissageUser() {
+      
+      localStorage.setItem("contact", JSON.stringify(contact));              
+    };
+    remplissageUser();
+  });
+
+  console.log(contact)
+  
+
+
+  
+/*
+
+
+
+
+let form = document.createElement("form");
+form.method="post";
+form.id="form";
+container.appendChild(form);
+
+
+
+let userOrder = {
+    contact: {}
+
+};
+let contact = {
+    
+    userName: document.getElementById("nameUser").value,
+    userFirstName: document.getElementById("firstNameUser").value,
+    userAddress: document.getElementById("adresseUser").value,
+    userCp: document.getElementById("cpUser").value,
+    userVille: document.getElementById("userVille").value,
+    userMail: document.getElementById("userMail").value,
+  };
+  
+alert(userOrder);}
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
